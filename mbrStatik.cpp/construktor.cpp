69de1afcefa2eb178e0e4e4b87522f7ab662fbfe
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-05-14 15:24:46 +0700

    memasukan string dan public

---
+++ mbrStatik.cpp/construktor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+	int nim;
+	string nama;
+
+public:
+	Mahasiswa()
+	{
+		nim = 0;
+		nama = "";
+	};
+	Mahasiswa(int iNim)
+	{
+
+	}
+};
