1afb29ab4c4c98648daa210ad8707cafd822327d
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-05-14 15:04:56 +0700

    memasukan string dan nama

--- mbrStatik.cpp/mbrStatikfunc.cpp
+++ mbrStatik.cpp/mbrStatikfunc.cpp
@@ -2,3 +2,23 @@
 #include <string>
 using namespace std;
 
+class mahasiswa
+{
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printALL();
+
+	static void setNim(int pNim) {nim = pNim;}
+	static int getNim() {return nim;}
+
+	mahasiswa(string pnama)
+	{
+		nama = pnama;
+		setID();
+	}
+};
